73c99fc2a96d3d09119fb1d3119f86a482db2f6b
SMHRD <SMHRD@DESKTOP-30J9BBU>
2020-06-23 17:44:55 +0900

    소스코드 추가

--- gitTest/src/HelloJava.java
+++ gitTest/src/HelloJava.java
@@ -1,9 +1,8 @@
-
 public class HelloJava {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("HelloJava");
+		System.out.println("Hello");
 	}
 
 }
